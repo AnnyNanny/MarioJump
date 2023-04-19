d6c7fe9ca6258ac2cc8c80e5821f15087cd04731
Anny <121063240+AnnyNanny@users.noreply.github.com>
2023-04-19 15:27:51 -0300

    Add files via upload

---
+++ MarioJump/js/script.js
@@ -0,0 +1,31 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+const jump = () => {
+    mario.classList.add('jump')
+    setTimeout(()=> {
+        mario.classList.remove('jump')
+    }, 500);
+}
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    console.log(marioPosition);
+
+    if (pipePosition <= 90 && pipePosition> 0 && marioPosition < 100) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './images/game-over.png';
+        mario.style.width = '100px';
+        mario.style.marginLeft = '20px'
+        
+        clearInterval(loop);
+    }
+}, 10);
+document.addEventListener('keydown', jump);
